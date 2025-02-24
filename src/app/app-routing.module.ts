6f07aaf81366c9a33ae8c8eee62b72edc52b0877
Eugen-Marian Sabău <cti22c104@cti.uoradea>
2025-02-24 09:41:44 +0200

    Explaining my changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalcComponent } from './components/calc/calc.component';
+import { CountryListComponent } from './components/country-list/country-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calc', component: CalcComponent},
+  { path: 'country-list', component: CountryListComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
