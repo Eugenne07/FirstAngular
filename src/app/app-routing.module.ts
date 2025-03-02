d8fd4e331c475d9b1b454ddfd241f922c229cb2c
Eugenne07 <sabaueugen02@gmail.com>
2025-03-02 15:46:25 +0200

    Add countries flag png

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalcComponent } from './components/calc/calc.component';
 import { CountryListComponent } from './components/country-list/country-list.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 const routes: Routes = [
-  { path: 'calc', component: CalcComponent},
-  { path: 'country-list', component: CountryListComponent}
+  { path: '', component: TopBarComponent },
+  { path: 'calc', component: CalcComponent },
+  { path: 'country-list', component: CountryListComponent },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
